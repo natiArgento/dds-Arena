21d4c353f253f44d8bbc191c24740e7674338d53
natiArgento <nati.argento51@gmail.com>
2014-08-21 12:24:42 -0300

    empiezo proyecto para prueba de concepto

---
+++ pruebaDeConcepto/src/main/java/dds2014/App.java
@@ -0,0 +1,13 @@
+package dds2014;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
